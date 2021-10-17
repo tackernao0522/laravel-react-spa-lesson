4cb3fe63a70e2c8dc9c6cd84556f2d717dfa7a38
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-17 18:24:25 +0900

    DBのpostsデータをフロント側で取得しステート(posts)にセット

--- server/resources/js/pages/Home.js
+++ server/resources/js/pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; // useEffectする
+import axios from 'axios';
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import MainTable from '../components/MainTable';
@@ -32,6 +33,26 @@ let rows = [
 function Home() {
     // 定義したスタイルを利用するための設定
     const classes = useStyles();
+
+    // postsの状態を管理する
+    const [posts, setPosts] = useState([]);
+
+    // 画面に到着したらgetPostsDataを呼ぶ
+    useEffect(() => {
+        getPostsData();
+    }, [])
+
+    // 一覧情報を取得しpostsにセットする
+    const getPostsData = () => {
+        axios
+            .get('/api/posts')
+            .then(response => {
+                setPosts(response.data);
+            })
+            .catch(() => {
+                console.log('通信に失敗しました');
+            })
+    }
 
     return (
         <div className="container">
